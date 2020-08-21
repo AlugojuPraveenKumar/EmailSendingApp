ae91e0b33bfffb1bd3926bbcb320982752c873b6
AlugojuPraveenKumar <69905889+AlugojuPraveenKumar@users.noreply.github.com>
2020-08-21 21:07:44 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {User} from './User';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'EmailSendingApp';
+  userModel = new User('','','')
+}
